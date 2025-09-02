aaffb4f25a4f2ed97db84ae9e3c5b0dcf0ba9162
asdelay <oleksandrptuha1@gmail.com>
2025-09-02 15:27:10 +0200

    feat: added theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,24 @@ import {
 function App() {
   const { mode, setMode } = useColorScheme();
   if (!mode) {
+    console.log(`mode is ${mode}`);
     return null;
   }
   return (
     <>
       <Container sx={{ height: "100vh" }}>
         <Typography variant="h1">Hello, world!</Typography>
+
         <Box sx={{ mt: 2, display: "flex", gap: 2 }}>
-          <Button
-            variant="outlined"
-            onClick={(e) => {
-              e.preventDefault();
-              setMode("dark");
-            }}
-          >
-            Dark theme
-          </Button>
-          <Button
-            variant="contained"
-            onClick={(e) => {
-              e.preventDefault();
-              setMode("light");
-            }}
-          >
-            Light theme
-          </Button>
+          {mode === "dark" ? (
+            <Button variant="contained" onClick={() => setMode("light")}>
+              Light theme
+            </Button>
+          ) : (
+            <Button variant="outlined" onClick={() => setMode("dark")}>
+              Dark theme
+            </Button>
+          )}
         </Box>
       </Container>
     </>
